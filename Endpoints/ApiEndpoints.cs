66115165d2361cd8769fd78300582d6af4b4664e
Garrett London <garrett.london2@gmail.com>
2023-11-05 14:53:51 -0500

    clean

--- Endpoints/ApiEndpoints.cs
+++ Endpoints/ApiEndpoints.cs
@@ -2,11 +2,13 @@
 
 public static class ApiEndpoints
 {
-	public static WebApplication MapApiEndpoints(this WebApplication app)
-	{
-		var api = app.MapGroup("/api");
+    public static WebApplication MapApiEndpoints(this WebApplication app)
+    {
+        var api = app.MapGroup("/api");
 
+        // maps to /api/example
+        api.MapGet("/example", ExampleHandler.Index);
 
         return app;
-	}
+    }
 }
