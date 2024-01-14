ca58fc5569cb32b12c5b7fc0c6ef0ce8a9720d69
ryumartinez <ryueduardomartinez@outlook.es>
2024-01-14 13:59:22 -0300

    created entities

--- Samples/BlazorMinimalApis.Mvc/Data/RambosaDbContext.cs
+++ Samples/BlazorMinimalApis.Mvc/Data/RambosaDbContext.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
 namespace BlazorMinimalApis.Mvc.Data;
-
+/*
+ * Para hacer migraciones correr:
+ * dotnet ef migrations add NombreDeLaMigracion
+ * dotnet ef database update
+ */
 public class RambosaDbContext : DbContext
 {
     public RambosaDbContext(DbContextOptions<RambosaDbContext> options) : base(options)
@@ -9,12 +13,27 @@ public class RambosaDbContext : DbContext
     }
     public DbSet<Blog> Blogs { get; set; }
     public DbSet<Post> Posts { get; set; }
-
-    public string DbPath { get; }
+    public DbSet<Plato> Platos { get; set; }
+    public DbSet<Cliente> Clientes { get; set; }
     
     /*protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlServer("Server=(local);Database=RambosaDb;Integrated Security=True;MultipleActiveResultSets=False;TrustServerCertificate=True");*/
 }
+
+public class Plato
+{
+    public int PlatoId { get; set; }
+    public int Precio { get; set; }
+}
+
+public class Cliente
+{
+    public int ClienteId { get; set; }
+    public string Nombre { get; set; }
+}
+
+//Un cliente puede pedir muchos platos. Un plato puede ser pedido por muchos clientes. 
+//La tabla intermedia se va a llamar pedidos. Es una relacion de muchos a muchos.
 
 public class Blog
 {
